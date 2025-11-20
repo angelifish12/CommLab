944248df5c9437bcd3a4008bf21db7d70f813976
angelifish12 <jc12237@nyu.edu>
2025-11-20 23:22:43 +0800

    g

---
+++ clock/script.js
@@ -0,0 +1,64 @@
+let secondsBox = document.querySelector(".seconds")
+let sound = document.querySelector("#ding");
+// let x = centerX + radius * Math.cos(angle);
+// let y = centerY + radius * Math.sin(angle);
+
+function getTheTime() {
+    let now = new Date(); //return the current moment
+
+    let h = now.getHours();      // 0–23
+    let m = now.getMinutes();    // 0–59
+    let s = now.getSeconds();    // 0–59
+
+
+    console.log(h, m, s);
+
+    let originalBird = document.querySelector(".original-bird");
+    let newBird = document.querySelector(".new-bird");
+
+    if (m == 0) {
+        originalBird.style.display = "none";
+        newBird.style.display = "block";
+    } else {
+        originalBird.style.display = "block";
+        newBird.style.display = "none";
+    }
+
+    // S
+    // clear out the second div completely
+    secondsBox.innerHTML = "";
+    //put divs into the seconds div to represent
+    //how many seconds the current moment "now" has
+    repeat(s, function () {
+        let div = document.createElement("div");
+        secondsBox.append(div);
+    })
+}
+
+// circle = document.getElementById('circleToMove');
+// centerX = 200;
+// centerY = 200;
+// radius = 100;
+// let angle = 0;
+// speed = 0.05;
+
+// function circle() {
+//     angle += speed;
+//     x = centerX + radius * Math.cos(angle);
+//     y = centerY + radius * Math.sin(angle);
+//     circle.style.left = x + 'px';
+//     circle.style.top = y + 'px';
+// }
+
+setInterval(getTheTime, 1000);
+getTheTime();
+
+
+
+
+// Leon's Helper function:
+function repeat(n, action) {
+    for (let i = 0; i < n; i++) {
+        action(i);
+    }
+}
